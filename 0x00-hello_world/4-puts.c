964ca106786e71db3fc7e5ffe3cc5c6ef2efd7b5
bbhicham <preeservice@gmail.com>
2023-07-16 15:42:00 +0100

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main() {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
+/**
+ * main - Entry point
+ *
+ * Description: A C program to print a specific sentence
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
