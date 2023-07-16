d2ec8939e9372ed21678d1750172798705de6a04
bbhicham <preeservice@gmail.com>
2023-07-16 21:17:57 +0100

    single-digit numbers.

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
-
 /**
  * main - Entry point
  *
- * Description: A C program rints all possible comb of single-digit numbers
+ * Description: A C program that prints all possible com of singledigit numbers
  *
  * Return: Always 0 (Success)
  */
@@ -15,8 +14,11 @@ int main(void)
 	for (i = 0; i < 10; i++)
 	{
 		putchar('0' + i);
-		putchar(',');
-		putchar(' ');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 
 	putchar('\n');
