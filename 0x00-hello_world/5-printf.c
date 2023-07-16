520f24b86a56d63bf8a7a3e2d9f0886c11d0d253
bbhicham <preeservice@gmail.com>
2023-07-16 15:49:49 +0100

    printf in c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program to print a specific sentence
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
