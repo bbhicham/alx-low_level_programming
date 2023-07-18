fc9eeb45b498fbb80f71bfdf567dfe7b01b92531
bbhicham <preeservice@gmail.com>
2023-07-18 01:42:50 +0100

    print com two

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,36 +9,36 @@
  */
 int main(void)
 {
-    int i;
-    int j;
-    int sum;
-    int t = 1;
+int i;
+int j;
+int sum;
+int t = 1;
 
-    for (i = 0; i < 10; i++)
-    {
-        for (j = 0; j < 10; j++)
-        {   
-            sum = i * 10 + j;
-            if (sum != 0)
-            {
-                if (sum % 10 == 0 && i > 0)
-                {
-                  j = t;
-                  t++;
-                }
-                else
-                {
-                  if (sum != 1)
-                  putchar(' ');
-                  putchar('0' + i);
-                  putchar('0' + j);
-                  if (sum != 89)
-                  putchar(','); 
-                }
-            }
-        }
-    }
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+sum = i * 10 + j;
+if (sum != 0)
+{
+if (sum % 10 == 0 && i > 0)
+{
+j = t;
+t++;
+}
+else
+{
+if (sum != 1)
+putchar(' ');
+putchar('0' + i);
+putchar('0' + j);
+if (sum != 89)
+putchar(',');
+}
+}
+}
+}
 
-    putchar('\n');
-    return (0);
+putchar('\n');
+return (0);
 }
