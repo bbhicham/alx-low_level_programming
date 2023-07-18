7dab3cd10ac61a07107c0e9235f6c3167b6ca51d
bbhicham <preeservice@gmail.com>
2023-07-18 01:54:41 +0100

    com two()

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -4,17 +4,20 @@ int main(void)
 {
     int num1, num2;
 
-    for (num1 = 0; num1 < 10; num1++)
+    for (num1 = 0; num1 < 100; num1++)
     {
-        for (num2 = 0; num2 < 10; num2++)
+        for (num2 = num1 + 1; num2 < 100; num2++)
         {
             putchar('0' + num1 / 10);
             putchar('0' + num1 % 10);
             putchar(' ');
             putchar('0' + num2 / 10);
             putchar('0' + num2 % 10);
-            putchar(',');
-            putchar(' ');
+            if (num1 != 98 || num2 != 99)
+            {
+                putchar(',');
+                putchar(' ');
+            }
         }
     }
 
