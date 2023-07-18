67a74717a1c916fc20c83a52d02f94d4a2064dff
bbhicham <preeservice@gmail.com>
2023-07-18 01:35:08 +0100

    print com two two

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,21 +15,24 @@ for (i = 0; i < 10; i++)
 {
 for (j = i + 1; j < 10; j++)
 {
-    for (k = j + 1; k < 10; k++)
-    {
-    putchar(' ');
-    putchar('0' + i);
-    putchar('0' + j);
-    putchar('0' + k);
+for (k = j + 1; k < 10; k++)
+{
+if (i != 0 && j != 1 && k != 2)
+{
+putchar(' ');
+}
+putchar('0' + i);
+putchar('0' + j);
+putchar('0' + k);
 
-    if (i != 7 || j != 8 || k != 9)
-    {
-    putchar(',');
-    }
-    }
+if (i != 7 || j != 8 || k != 9)
+{
+putchar(',');
+}
+}
 }
 }
 
-printf("\n");
+putchar('\n');
 return (0);
 }
