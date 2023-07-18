3b640aa277d16d5ffb11e30bb542efbaa5ab57b8
bbhicham <preeservice@gmail.com>
2023-07-19 00:57:27 +0100

    pro_alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet - a-z
+ * @c: f
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_alphabet(void)
+{
+char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'q' && letter != 'e')
+		{
+			_putchar(letter);
+		}
+	}
+
+	_putchar('\n');
+}
