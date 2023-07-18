0cd5be019553521814558d7311e2bd08ee314659
bbhicham <preeservice@gmail.com>
2023-07-18 23:45:53 +0100

    putchar function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <main.h>
+
+int main()
+{
+    int i;
+    char put[] = "_putchar";
+     
+    for ( i = 0; i < 8; i++)
+    {
+        _putchar(put[i]);
+    }
+
+    _putchar('\n');
+    return (0);
+}
