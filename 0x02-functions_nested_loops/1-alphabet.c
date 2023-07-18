b45ed875333d91cc51f7bb49ed54be97aeec06f1
bbhicham <preeservice@gmail.com>
2023-07-19 00:59:39 +0100

    pro_alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -11,13 +11,10 @@ void print_alphabet(void)
 {
 char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-	{
-		if (letter != 'q' && letter != 'e')
-		{
-			_putchar(letter);
-		}
-	}
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+_putchar(letter);
+}
 
-	_putchar('\n');
+_putchar('\n');
 }
