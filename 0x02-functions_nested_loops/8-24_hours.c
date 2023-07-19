514382d0f3aed9a224fc23a09821b7c904f3feb2
bbhicham <preeservice@gmail.com>
2023-07-19 18:16:00 +0100

    jack

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,17 +8,19 @@ void jack_bauer(void)
 int h=0;
 int m=0;
 
-while (h<24)
+while (h < 24)
 {
-while (m<60)
+while (m < 60)
 {
-_putchar((h/10) + '0');
-_putchar((h%10) + '0');
+_putchar((h / 10) + '0');
+_putchar((h % 10) + '0');
 _putchar(':');
-_putchar((m/10) + '0');
-_putchar((m%10) + '0');
-}
+_putchar((m / 10) + '0');
+_putchar((m % 10) + '0');
 _putchar('\n');
+m++;
+}
+h++;
 }
 
 }
