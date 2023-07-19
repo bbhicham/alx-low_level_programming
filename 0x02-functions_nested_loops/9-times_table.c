8b121377f689643d01df9ad13b5d7edf54f7727e
bbhicham <preeservice@gmail.com>
2023-07-19 20:29:59 +0100

    time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,37 +10,31 @@ int j;
 
 for (i = 0; i < 10; i++)
 {
-    for ( j = 0; j < 10; j++)
-    {
-        int mult = i * j; 
-         
-        if (mult < 10)
-        {
-            if (j != 0 )
-        {
-           _putchar(' ');
-            _putchar(' ');
-        }
-           
-            _putchar(mult + '0');
-          
-        }
-        else
-        {  
-            _putchar(' ');
-            _putchar((mult/10+'0'));
-            _putchar((mult%10)+'0');
-        }
-          if (j != 9)
-        {
-           _putchar(',');
-           
-        }
-        
-        
-        
-    }
-     _putchar('\n');
+for (j = 0; j < 10; j++)
+{
+int mult = i * j;
+
+if (mult < 10)
+{
+if (j != 0)
+{
+_putchar(' ');
+_putchar(' ');
+}
+_putchar(mult + '0');
+}
+else
+{
+_putchar(' ');
+_putchar((mult / 10) + '0');
+_putchar((mult % 10) + '0');
+}
+if (j != 9)
+{
+_putchar(',');
+}
+}
+_putchar('\n');
 }
 
 
