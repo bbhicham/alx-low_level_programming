1726830f6c91a6a66a68a251ea545ce02ccc94cc
bbhicham <preeservice@gmail.com>
2023-07-19 21:56:16 +0100

    100 time

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,8 +6,8 @@
 void print_times_table(int n) {
   int i;
   int j;
-
-  for (i = 0; i <= n; i++) {
+  if(n<15 || n>0){
+    for (i = 0; i <= n; i++) {
     for (j = 0; j <= n; j++) {
       int mult = i * j;
 
@@ -35,5 +35,7 @@ void print_times_table(int n) {
     }
     _putchar('\n');
   }
+  }
+  
 
 }
