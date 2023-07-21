7f1b972576968b7e8c7b76c8750c3409453d1d0c
bbhicham <preeservice@gmail.com>
2023-07-21 18:55:08 +0100

    is digital

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper- check if char is digital
+ * @c: is the char => to be checked
+ * Return: 1 if charcter is digital and 0 is otherwise.
+ */
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+
+}
