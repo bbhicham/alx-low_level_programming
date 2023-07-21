4f505cd4279eb8e53a91661f8eb7a82080ee1d0c
bbhicham <preeservice@gmail.com>
2023-07-21 20:51:28 +0100

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a square.
+ * @size: size of sq
+ */
+void print_square(int size)
+{
+int i, j;
+
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+_putchar('#');
+}
+
+if (i != (size - 1))
+{
+_putchar('\n');
+}
+
+}
+
+_putchar('\n');
+}
