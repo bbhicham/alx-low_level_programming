d905ada3fe69f9570e3a6731744eb3ec0858e5d0
bbhicham <preeservice@gmail.com>
2023-07-21 19:12:45 +0100

    numbers and signs

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9,unless 2 and 4 ,
+ */
+void print_most_numbers(void)
+{
+int i;
+i = 0;
+
+for (i = 0; i < 10; i++)
+{
+if (i != 2 || i != 4)
+{
+_putchar('0' + i);
+}
+
+}
+_putchar('\n');
+}
