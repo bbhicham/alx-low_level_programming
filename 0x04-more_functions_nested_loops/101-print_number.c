fc3ec05b17c0da8ec30963fd27bc6721b50b316e
bbhicham <preeservice@gmail.com>
2023-07-22 00:51:56 +0100

    print any int

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,61 +1,59 @@
 #include "main.h"
 
+/**
+ * print_number - print any int
+ * @n: n int well be printed
+ */
 
 void print_number(int n)
 {
-
-  int length = 0;
-  unsigned int number;
-  int i;
-  int p;
-  int j;
- 
-    if (n < 0)
-	   {
-	    _putchar('-');
-	      number = (unsigned int)(-n);
-	     
-	   }else{
-	       number = n;
-	   }
- 
-  if (number == 0)
-  {
-      length = 1;
-  }
-  else
-  {
-      while (number != 0)
-     	{
-    	  number /= 10;
-    	  length++;
-      	}
-  }
-  
-   if (n < 0)
-	   {
-	      number = (unsigned int)(-n);
-	     
-	   }else{
-	       number = n;
-	   }
- 
-  for (i = length-1 ; i >=0; i--) {
-      p =1;
-      if(i >=1){
-        for (j = 0; j < i; j++) {
-            p *= 10; 
-        }
-      
-        if(i==length-1){
-         _putchar('0' + (number /p)); 
-        }else{
-         _putchar('0' + ((number /p)%10));
-        }
-        
-      }else{
-        _putchar('0' + (number  % 10)) ;
-      }
-  }
-  
+int length = 0;
+unsigned int number, len;
+int i, p, j;
+if (n < 0)
+{
+_putchar('-');
+number = (unsigned int)(-n);
+len = (unsigned int)(-n);
+}
+else
+{
+number = n;
+len = n;
+}
+if (len == 0)
+{
+length = 1;
+}
+else
+{
+while (len != 0)
+{
+len /= 10;
+length++;
+}
+}
+for (i = length - 1 ; i >= 0; i--)
+{
+p = 1;
+if (i >= 1)
+{
+for (j = 0; j < i; j++)
+{
+p *= 10;
+}
+if (i == length - 1)
+{
+_putchar('0' + (number / p));
+}
+else
+{
+putchar('0' + ((number / p) % 10));
+}
+}
+else
+{
+_putchar('0' + (number  % 10));
+}
+}
 }
