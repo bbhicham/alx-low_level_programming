9dfd4d30f4fcb9ca01be92f992f4fce6d3451476
bbhicham <preeservice@gmail.com>
2023-07-21 19:25:19 +0100

    prints 10 times the numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,7 +12,16 @@ for (i = 0; i < 11; i++)
 {
 for (j = 0; j <= 14; j++)
 {
-_putchar('0' + i);
+if (j >= 10)
+{
+_putchar('0' + j / 10);
+_putchar('0' + j % 10);
+}
+else
+{
+_putchar('0' + j);
+}
+
 }
 _putchar('\n');
 }
