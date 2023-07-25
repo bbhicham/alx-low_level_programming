164c795e47addfee3801852421292499a3e336df
bbhicham <preeservice@gmail.com>
2023-07-25 19:19:17 +0100

    put 2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  * puts2 - prints every other character of a string,
- * @s: input string to printed
+ * @str: input string to printed
  */
-void puts2 (char *str)
+void puts2(char *str)
 {
 while (*str != '\0')
 {
