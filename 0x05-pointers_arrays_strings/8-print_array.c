c6005aa4faa853d9179c82681739c414106b575a
bbhicham <preeservice@gmail.com>
2023-07-25 21:41:02 +0100

    print arrray

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - print item of array
+ * @a: addresse of array
+ * @n: lenght of array
+ */
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n ; i++)
+{
+if (i != n - 1)
+{
+printf("%d, ", a[i]);
+}
+else
+{
+printf("%d", a[i]);
+}
+}
+_putchar('\n');
+}
