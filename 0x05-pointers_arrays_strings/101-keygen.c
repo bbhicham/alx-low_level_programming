394f17bf617f8e9206ef8d16c6713ea5aca9a2d4
bbhicham <preeservice@gmail.com>
2023-07-26 01:47:51 +0100

    generete password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,33 +2,26 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define PASSWORD_LENGTH 14
-
-char generate_random_char() {
+/**
+ * main - program that generates random valid passwords
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
     const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    int index = rand() % (sizeof(charset) - 1);
-    return charset[index];
-}
-
-int is_valid_password(const char *password) {
-    const char *target_password = "randompw";
-    return (strcmp(password, target_password) == 0);
-}
-
-int main(void) {
-    char password[PASSWORD_LENGTH + 1];
+    const int password_length = 12;
+    char password[password_length + 1];
     srand(time(NULL));
 
-    do {
-        for (int i = 0; i < PASSWORD_LENGTH; i++) {
-            password[i] = generate_random_char();
-        }
-        password[PASSWORD_LENGTH] = '\0';
-        if (1) {
-            printf("Found valid password: %s\n", password);
-            break;
-        }
-    } while (1);
+    for (int i = 0; i < password_length; i++)
+    {
+        int index = rand() % (sizeof(charset) - 1);
+        password[i] = charset[index];
+    }
+
+    password[password_length] = '\0';
+    printf("%s\n", password);
 
     return 0;
 }
