27a2b875a3ef67a840ae7cc9feccacd32317abb5
bbhicham <preeservice@gmail.com>
2023-07-28 23:06:09 +0100

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcmp - that compares two strings.
+ * @s1: string pointed 1
+ * @s1: string pointed 2
+
+ * Return: return int
+ */
+int _strcmp(char *s1, char *s2)
+{
+int e = 0;
+int i=0;
+while (s1[i])
+{
+if (s1[i] != s2[i])
+{
+e = ((int)s1[i]-48) - ((int)s2[i]-48) ;
+}
+i++;
+}
+return (e);
+}
