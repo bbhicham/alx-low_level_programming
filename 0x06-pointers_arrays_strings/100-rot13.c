b924d9a24231f48825dedaac0e1277cbe9bd3a55
bbhicham <preeservice@gmail.com>
2023-07-29 01:22:25 +0100

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,29 +1,30 @@
 #include "main.h"
 
 /**
- * rot13 - tthat encodes a string into 1337.
+ * rot13 - tthat encodes a string rot13.
  * @str: input String.
  *
  * Return: A pointer to the changed string.
  */
-char *rot13(char *str)
-{
-char *result = str;
+char *rot13(char *str) {
+    char *result = str;
+    char alph[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    char arot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-while (*str) {
-char c = *str;
+    while (*str) {
+        char c = *str;
+        char *pos = alph;
 
-if ((c >= 'a' && c <= 'z'))
-{
-*str = ((c - 'a' + 13) % 26) + 'a';
-}
-else if ((c >= 'A' && c <= 'Z'))
-{
-*str = ((c - 'A' + 13) % 26) + 'A';
-}
+        while (*pos && *pos != c) {
+            pos++;
+        }
 
-str++;
-}
+        if (*pos) {
+            *str = arot[pos - alph];
+        }
 
-return (result);
+        str++;
+    }
+
+    return result;
 }
