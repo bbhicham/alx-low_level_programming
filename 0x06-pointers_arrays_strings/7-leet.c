ba44ac504b7f35872e75d62db7b629e76719aa51
bbhicham <preeservice@gmail.com>
2023-07-29 01:01:29 +0100

    1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,17 +1,18 @@
 
 char *leet(char *str) {
     char replacements[256] = {0};
+    int i;
     replacements['a'] = replacements['A'] = '4';
     replacements['e'] = replacements['E'] = '3';
     replacements['o'] = replacements['O'] = '0';
     replacements['t'] = replacements['T'] = '7';
     replacements['l'] = replacements['L'] = '1';
 
-    for (int i = 0; str[i] != '\0'; i++) {
+    for (i = 0; str[i] != '\0'; i++) {
         if (replacements[(unsigned char)str[i]]) {
             str[i] = replacements[(unsigned char)str[i]];
         }
     }
 
-    return str;
+    return (str);
 }
