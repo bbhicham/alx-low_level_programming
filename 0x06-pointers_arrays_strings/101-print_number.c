129d54365cb314bc29e5c1bd5835c5916883cd54
bbhicham <preeservice@gmail.com>
2023-07-29 01:38:42 +0100

    print any number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,22 +1,25 @@
 #include "main.h"
 
 /**
- * rot13 - that prints an integer
- * @str: input int to be print
+ * print_number - that prints any integer
+ * @n: input int to be print
  */
-void print_number(int n) {
-    unsigned int n1;
+void print_number(int n)
+{
+unsigned int n1;
 
-	n1 = n;
+n1 = n;
 
-    if (n < 0) {
-        _putchar('-');
-        n1 = -n;
-    }
+if (n < 0)
+{
+_putchar('-');
+n1 = -n;
+}
 
-    if (n / 10 != 0) {
-        print_number(n1 / 10);
-    }
+if (n / 10 != 0)
+{
+print_number(n1 / 10);
+}
 
-    _putchar(n1 % 10 + '0');
+_putchar(n1 % 10 + '0');
 }
