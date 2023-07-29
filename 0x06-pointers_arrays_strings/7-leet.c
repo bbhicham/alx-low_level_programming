4c10d90ef7d2af8802f2ba010fba8eee91457308
bbhicham <preeservice@gmail.com>
2023-07-29 01:04:10 +0100

    1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,18 +1,28 @@
+#include "main.h"
 
-char *leet(char *str) {
-    char replacements[256] = {0};
-    int i;
-    replacements['a'] = replacements['A'] = '4';
-    replacements['e'] = replacements['E'] = '3';
-    replacements['o'] = replacements['O'] = '0';
-    replacements['t'] = replacements['T'] = '7';
-    replacements['l'] = replacements['L'] = '1';
+/**
+ * leet - tthat encodes a string into 1337.
+ * @str: input String.
+ *
+ * Return: A pointer to the changed string.
+ */
+char *leet(char *str)
+{
+char replacements[256] = {0};
+int i;
+replacements['a'] = replacements['A'] = '4';
+replacements['e'] = replacements['E'] = '3';
+replacements['o'] = replacements['O'] = '0';
+replacements['t'] = replacements['T'] = '7';
+replacements['l'] = replacements['L'] = '1';
 
-    for (i = 0; str[i] != '\0'; i++) {
-        if (replacements[(unsigned char)str[i]]) {
-            str[i] = replacements[(unsigned char)str[i]];
-        }
-    }
+for (i = 0; str[i] != '\0'; i++)
+{
+if (replacements[(unsigned char)str[i]])
+{
+str[i] = replacements[(unsigned char)str[i]];
+}
+}
 
-    return (str);
+return (str);
 }
