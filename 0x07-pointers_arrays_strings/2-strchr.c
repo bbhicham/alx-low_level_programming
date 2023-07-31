511a29d4e793a89a2884e55709171fe7dd8a7da9
bbhicham <preeservice@gmail.com>
2023-07-31 22:58:27 +0100

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -6,18 +6,18 @@
  * @s: input
  * @c:input
  *
- * Return: first occurrence of the character c in the string s, or NULL if the character
+ * Return: first occurrence of the char c in the string s, or NULL
  */
 
 char *_strchr(char *s, char c)
 {
 int i;
 
-for (i = 0; s[i] == '\0'  ; i++)
+for (i = 0; s[i] >= '\0'  ; i++)
 {
-if(s[i]==c)
+if (s[i] == c)
 {
-return(s + i);
+return (s + i);
 }
 
 }
