8863fedc8c9b793b4db4ce1005032d387e900838
bbhicham <preeservice@gmail.com>
2023-08-01 18:11:25 +0100

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+#include <stdio.h>
+/**
+ * _strspn -  length of a prefix substring
+ * @s: input
+ * @accept:input
+ *
+ * Return: length of a prefix substring
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int i, j;
+unsigned int c = 0;
+for (int i = 0; s[i]; i++)
+{
+for (int j = 0; accept[j]; j++)
+{
+if (accept[j] == s[i])
+{
+c++;
+break;
+}
+else if (accept[j + 1] == '\0')
+{
+return (c);
+}
+}
+}
+return (c);
+}
