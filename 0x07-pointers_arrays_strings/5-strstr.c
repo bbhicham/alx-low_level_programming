bb80c0dda8010e7b95676786e8864cd131869819
bbhicham <preeservice@gmail.com>
2023-08-01 18:54:55 +0100

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strstr - Entry point
+ * @haystack: input
+ * @needle: input
+ * Return: beginning of the located substring, or NULL 
+ */
+char *_strstr(char *haystack, char *needle)
+{
+for (; *haystack != '\0'; haystack++)
+{
+char *ta = haystack;
+char *tb = needle;
+
+		while (*ta == *tb && *tb != '\0')
+		{
+			ta++;
+			tb++;
+		}
+
+		if (*tb == '\0')
+			return (haystack);
+	}
+
+	return (0);
+}
