37bf6854655034e93bf40ae1ee76d7778b1e1acd
bbhicham <preeservice@gmail.com>
2023-08-08 01:27:33 +0100

    grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,29 +12,25 @@ int **alloc_grid(int width, int height)
     int **grid;
     int i, j;
 
-    // Check if width or height is 0 or negative
+ 
     if (width <= 0 || height <= 0)
         return NULL;
 
-    // Allocate memory for rows (height)
     grid = (int **)malloc(height * sizeof(int *));
     if (grid == NULL)
         return NULL;
 
-    // Allocate memory for each row (width) and initialize elements to 0
     for (i = 0; i < height; i++)
     {
         grid[i] = (int *)malloc(width * sizeof(int));
         if (grid[i] == NULL)
         {
-            // If allocation fails, free previously allocated memory and return NULL
             for (j = 0; j < i; j++)
                 free(grid[j]);
             free(grid);
             return NULL;
         }
 
-        // Initialize elements to 0
         for (j = 0; j < width; j++)
             grid[i][j] = 0;
     }
