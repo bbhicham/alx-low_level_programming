702170103d3e9cca05c984f866574c04ad85cbe2
bbhicham <preeservice@gmail.com>
2023-08-08 02:25:54 +0100

    strtow

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,53 +1,58 @@
 #include <stdlib.h>
-#include <string.h>
+#include "main.h"
 
 /**
- * strtow - Splits a string into words.
- * @str: The input string to be split.
+ * **strtow - Splits a string into words.
+ * @str: The input string to split.
  *
- * Return: Pointer to an array of strings (words), or NULL if str is NULL or empty.
+ * Return: Pointer to an array of strings (words), or NULL if str is NULL, empty, or memory allocation fails.
  */
 char **strtow(char *str)
 {
-    char **words = NULL;
-    char *token = NULL;
-    int i, j, word_count = 0;
+    char **words, *word;
+    int i, k = 0, len = 0, num_words = 0, start, end;
 
-   
+    
     if (str == NULL || *str == '\0')
         return NULL;
 
-    
-    for (i = 0; str[i]; i++)
-    {
-        if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
-            word_count++;
-    }
+   
+    while (*(str + len))
+        len++;
 
    
-    words = (char **)malloc((word_count + 1) * sizeof(char *));
+    for (i = 0; i <= len; i++)
+    {
+        if (str[i] == ' ' || str[i] == '\0')
+        {
+            if (num_words > 0)
+            {
+                end = i;
+                word = (char *)malloc(sizeof(char) * (k + 1));
+                if (word == NULL)
+                {
+                   
+                    for (int j = 0; j < num_words; j++)
+                        free(words[j]);
+                    free(words);
+                    return NULL;
+                }
+                while (start < end)
+                    *word++ = str[start++];
+                *word = '\0';
+                words[num_words - 1] = word - k;
+                k = 0;
+            }
+        }
+        else if (k++ == 0)
+            start = i;
+    }
+
+    words = (char **)malloc((num_words + 1) * sizeof(char *));
     if (words == NULL)
         return NULL;
 
-    
-    token = strtok(str, " ");
-    i = 0;
-    while (token != NULL)
-    {
-        words[i] = strdup(token);
-        if (words[i] == NULL)
-        {
-           
-            for (j = 0; j < i; j++)
-                free(words[j]);
-            free(words);
-            return NULL;
-        }
-        token = strtok(NULL, " ");
-        i++;
-    }
+    words[num_words] = NULL;
 
-    words[word_count] = NULL;
-
-    return (words);
+    return words;
 }
