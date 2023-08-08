291d69b745a79fa996dc7e6541095512e1612aa4
bbhicham <preeservice@gmail.com>
2023-08-08 01:37:19 +0100

    free grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+/**
+ * free_grid - Deallocates the memory of a 2D grid created by alloc_grid.
+ * @grid: The address of the 2D array.
+ * @height: The height of the 2D array.
+ *
+ * Return: Nothing.
+ */
+void free_grid(int **grid, int height)
+{
+    int i;
+
+
+    if (grid != NULL)
+    {
+       
+        for (i = 0; i < height; i++)
+        {
+            free(grid[i]);
+        }
+
+        
+        free(grid);
+    }
+}
