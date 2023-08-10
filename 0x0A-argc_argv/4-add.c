4fa0ca2443122dccda3926779074fcf0ef9ad1d4
bbhicham <preeservice@gmail.com>
2023-08-10 03:02:59 +0100

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,25 +8,28 @@
  * Description: Print the result, followed by a new line
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[]) {
-    int sum = 0;
-    int i;
-    for (i = 1; i < argc; i++) {
-        int num = 0;
-        int j = 0;
-        
-        while (argv[i][j] != '\0') {
-            if (!isdigit(argv[i][j])) {
-                printf("Error\n");
-                return 1;
-            }
-            j++;
-        }
-        
-        num = atoi(argv[i]);
-        sum += num;
-    }
-    
-    printf("%d\n", sum);
-    return 0;
+int main(int argc, char *argv[])
+{
+int sum = 0;
+int i;
+for (i = 1; i < argc; i++)
+{
+int num = 0;
+int j = 0;
+
+while (argv[i][j] != '\0')
+{
+if (!isdigit(argv[i][j]))
+{
+printf("Error\n");
+return (1);
+}
+j++;
+}
+
+num = atoi(argv[i]);
+sum += num;
+}
+printf("%d\n", sum);
+return (0);
 }
