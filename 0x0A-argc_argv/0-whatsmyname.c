9ca40327da883a827a5f0e63e61ac22528d163eb
bbhicham <preeservice@gmail.com>
2023-08-10 02:09:56 +0100

    my name is

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: is a count of the arguments supplied to the program
+ * @argv:  is an array of pointers to the strings which are those arguments
+ * Description: A C program prints its name, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char const *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
