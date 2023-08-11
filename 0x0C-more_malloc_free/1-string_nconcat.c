1671319711d20c15771ca29e655e77d9df7d7f2f
bbhicham <preeservice@gmail.com>
2023-08-11 02:03:41 +0100

    concatenated

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,36 +10,36 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    unsigned int len1 = 0, len2 = 0, total_len = 0;
-    char *concatenated;
-    unsigned int i;
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+unsigned int len1 = 0, len2 = 0, total_len = 0;
+char *concatenated;
+unsigned int i;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
 
-    while (s1[len1] != '\0')
-        len1++;
+while (s1[len1] != '\0')
+len1++;
 
-    while (s2[len2] != '\0')
-        len2++;
+while (s2[len2] != '\0')
+len2++;
 
-    if (n >= len2)
-        n = len2;
+if (n >= len2)
+n = len2;
 
-    total_len = len1 + n;
+total_len = len1 + n;
 
-    concatenated = malloc(sizeof(char) * (total_len + 1));
-    if (concatenated == NULL)
-        return NULL;
+concatenated = malloc(sizeof(char) * (total_len + 1));
+if (concatenated == NULL)
+return (NULL);
 
-    for (i = 0; i < len1; i++)
-        concatenated[i] = s1[i];
+for (i = 0; i < len1; i++)
+concatenated[i] = s1[i];
 
-    for (i = 0; i < n; i++)
-        concatenated[len1 + i] = s2[i];
+for (i = 0; i < n; i++)
+concatenated[len1 + i] = s2[i];
 
-    concatenated[total_len] = '\0';
+concatenated[total_len] = '\0';
 
-    return concatenated;
+return (concatenated);
 }
