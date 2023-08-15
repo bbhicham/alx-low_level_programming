1b83533509361c54e332a1d3274f6e1344531104
bbhicham <preeservice@gmail.com>
2023-08-16 00:19:49 +0100

    new  dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,6 +16,8 @@ struct dog
 	char *owner;
 };
 
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
